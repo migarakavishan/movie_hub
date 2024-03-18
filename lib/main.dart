08035db9c3bce72e342aca25674a1fc60b90cc8a
migarakavishan <migarakavishan43@gmail.com>
2024-03-19 02:45:33 +0530

    splash screen add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:movie_hub/time_api/screens/homepage.dart';
+import 'package:movie_hub/screens/splash_screen.dart';
+// import 'package:movie_hub/time_api/screens/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const SplashScreen(),
     );
   }
 }
