01ebe7ddd285a56a551da0e6667d80e5f14287a2
migarakavishan <migarakavishan43@gmail.com>
2024-03-20 22:32:52 +0530

    carouselSlider add

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -3,20 +3,22 @@ class MovieModel {
   String overview;
   String backdropPath;
   String posterPath;
+  double voteAverage;
 
   MovieModel({
     required this.title,
     required this.overview,
     required this.backdropPath,
     required this.posterPath,
+    required this.voteAverage
   });
 
   factory MovieModel.fromJson(Map<String, dynamic> json) {
     return MovieModel(
         title: json['title'],
         overview: json['overview'],
+        voteAverage: json['vote_average'],
         backdropPath: "https://image.tmdb.org/t/p/w500${json['backdrop_path']}",
-        posterPath: "https://image.tmdb.org/t/p/w500${json['poster_path']}"
-      );
+        posterPath: "https://image.tmdb.org/t/p/w500${json['poster_path']}");
   }
 }
