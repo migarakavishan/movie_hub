23ad0abb1f5f5ed4865bdc45e1d669fe3009592c
migarakavishan <migarakavishan43@gmail.com>
2024-03-21 14:34:28 +0530

    now playing add success

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -151,32 +151,98 @@ class _HomePageState extends State<HomePage> {
                       fontWeight: FontWeight.bold),
                 ),
                 const Divider(),
-                SizedBox(
-                  height: 115,
-                  child: ListView.builder(
-                      itemCount: 3,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 250,
-                            height: 110,
-                            decoration: BoxDecoration(
-                                color: Colors.grey.shade900,
-                                borderRadius: BorderRadius.circular(15)),
-                            child: Row(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                    child: Image.network(
-                                        'https://image.tmdb.org/t/p/w500/wkfG7DaExmcVsGLR4kLouMwxeT5.jpg'))
-                              ],
-                            ),
-                          ),
-                        );
-                      }),
-                )
+                FutureBuilder(
+                    future: ApiServices().getNowPlayingMovies(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      if (snapshot.hasError) {
+                        return const Text("Something went wrong");
+                      }
+                      List<MovieModel> movies = snapshot.data!;
+                      return SizedBox(
+                        height: 115,
+                        child: ListView.builder(
+                            itemCount: movies.length,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SizedBox(
+                                  width: 220,
+                                  child: Container(
+                                    width: 220,
+                                    height: 110,
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey.shade900,
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            child: Image.network(
+                                                movies[index].posterPath)),
+                                        const SizedBox(
+                                          width: 5,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              movies[index].title,
+                                              style: TextStyle(
+                                                  color: Colors.grey.shade300),
+                                            ),
+                                            Container(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal: 8),
+                                                decoration: BoxDecoration(
+                                                    color: Colors.deepOrange
+                                                        .withOpacity(0.4),
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            20)),
+                                                child: Row(
+                                                  children: [
+                                                    Text(
+                                                      movies[index].voteAverage
+                                                          .toString()
+                                                          .substring(0, 3),
+                                                      style: const TextStyle(
+                                                          color: Colors.white,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          fontSize: 12),
+                                                    ),
+                                                    const SizedBox(
+                                                      width: 5,
+                                                    ),
+                                                    Icon(
+                                                      Icons.star,
+                                                      color: Colors
+                                                          .yellow.shade600,
+                                                      size: 14,
+                                                    )
+                                                  ],
+                                                ))
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }),
+                      );
+                    }),
+                    
               ],
             ),
           ),
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,6 +7,7 @@ import 'package:movie_hub/models/movie_model.dart';
 class ApiServices {
   final apiKey = 'api_key=2058ec167f96bdbcd73b462bd88f128e';
   final propularMovies = 'https://api.themoviedb.org/3/movie/popular?';
+  final nowPlaying = 'https://api.themoviedb.org/3/movie/now_playing?';
 
   Future<List<MovieModel>> getPopularMovies() async {
     final response = await http.get(Uri.parse('$propularMovies$apiKey'));
@@ -18,6 +19,21 @@ class ApiServices {
           .toList();
       Logger().f(movies[0].posterPath);
 
+      return movies;
+    } else {
+      Logger().e('Error - ${response.statusCode}');
+      return [];
+    }
+  }
+
+  Future<List<MovieModel>> getNowPlayingMovies() async {
+    final response = await http.get(Uri.parse('$nowPlaying$apiKey'));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = jsonDecode(response.body);
+      List<dynamic> results = body['results'];
+      List<MovieModel> movies = results
+          .map((movie) => MovieModel.fromJson(movie as Map<String, dynamic>))
+          .toList();
       return movies;
     } else {
       Logger().e('Error - ${response.statusCode}');
