28fda67e8dda361d4606d13972cc212ba08372aa
migarakavishan <migarakavishan43@gmail.com>
2024-03-24 02:57:47 +0530

    companies data add

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,3 +1,5 @@
+import 'package:movie_hub/models/company_model.dart';
+
 class MovieModel {
   String title;
   String overview;
@@ -8,6 +10,7 @@ class MovieModel {
   bool isAdult;
   String releaseDate;
   String? tagline;
+  List<CompanyModel>? companies;
 
   MovieModel(
       {required this.title,
@@ -18,9 +21,16 @@ class MovieModel {
       required this.id,
       required this.isAdult,
       required this.releaseDate,
-      this.tagline});
+      this.tagline,
+      this.companies});
 
   factory MovieModel.fromJson(Map<String, dynamic> json) {
+    List<CompanyModel>? companies = json['production_companies'] != null
+        ? (json['production_companies'] as List<dynamic>)
+            .map((company) =>
+                CompanyModel.fromJson(company as Map<String, dynamic>))
+            .toList()
+        : null;
     return MovieModel(
         title: json['title'],
         overview: json['overview'],
@@ -29,6 +39,7 @@ class MovieModel {
         isAdult: json['adult'],
         releaseDate: json['release_date'],
         tagline: json['tagline'],
+        companies: companies,
         backdropPath: "https://image.tmdb.org/t/p/w500${json['backdrop_path']}",
         posterPath: "https://image.tmdb.org/t/p/w500${json['poster_path']}");
   }
